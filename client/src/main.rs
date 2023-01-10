a8a920dfa21e9870ef4c8e08738a21f80215ed4a
MysteryCoder456 <rehatbir.singh@gmail.com>
2023-01-10 19:03:19 +0400

    switched to bevy 0.9 and renet

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,5 +1,13 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            window: WindowDescriptor {
+                title: "Test".to_owned(),
+                ..Default::default()
+            },
+            ..Default::default()
+        }))
+        .run();
 }
