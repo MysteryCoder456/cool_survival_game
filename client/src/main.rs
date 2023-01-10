4e41d6c10343529229fbd0ecb1e18be1da7b1bac
MysteryCoder456 <rehatbir.singh@gmail.com>
2023-01-10 21:13:14 +0400

    added client side netcode config

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,4 +1,8 @@
+use shared::PROTOCOL_ID;
+use std::{net::UdpSocket, time::SystemTime};
+
 use bevy::prelude::*;
+use bevy_renet::*;
 
 fn main() {
     App::new()
@@ -9,5 +13,27 @@ fn main() {
             },
             ..Default::default()
         }))
+        .add_plugin(RenetClientPlugin::default())
+        .insert_resource(create_renet_config())
         .run();
+}
+
+fn create_renet_config() -> renet::RenetClient {
+    let current_time = SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .unwrap();
+
+    let server_addr = "127.0.0.1:5678".parse().unwrap();
+    let socket = UdpSocket::bind(server_addr).unwrap();
+
+    let config = renet::RenetConnectionConfig::default();
+
+    let authentication = renet::ClientAuthentication::Unsecure {
+        protocol_id: PROTOCOL_ID,
+        client_id: current_time.as_millis() as u64,
+        server_addr,
+        user_data: None,
+    };
+
+    renet::RenetClient::new(current_time, socket, config, authentication).unwrap()
 }
--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -1,14 +1,2 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+pub const PROTOCOL_ID: u64 = 1;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
