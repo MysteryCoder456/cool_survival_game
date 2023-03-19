a3cf37dd9911e73406214d185dba0f803da75c4d
MysteryCoder456 <rehatbir.singh@gmail.com>
2023-03-19 18:53:52 +0400

    added orc setup code

--- client/src/main_game/orc.rs
+++ client/src/main_game/orc.rs
@@ -1,5 +1,7 @@
 use bevy::prelude::*;
 
+use crate::GameState;
+
 pub mod events {
     use bevy::prelude::Vec2;
 
@@ -16,11 +18,38 @@ pub mod events {
     }
 }
 
+#[derive(Resource)]
+struct OrcAssets {
+    idle: Handle<TextureAtlas>,
+}
+
 pub struct OrcPlugin;
 
 impl Plugin for OrcPlugin {
     fn build(&self, app: &mut App) {
         app.add_event::<events::SpawnOrc>()
-            .add_event::<events::OrcTransformUpdate>();
+            .add_event::<events::OrcTransformUpdate>()
+            .add_startup_system(setup_orc)
+            .add_system_set(SystemSet::on_update(GameState::Game).with_system(spawn_orc_system));
+    }
+}
+
+fn setup_orc(
+    mut commands: Commands,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+    asset_server: Res<AssetServer>,
+) {
+    let idle_texture: Handle<Image> = asset_server.load("textures/orc/idle.png");
+    let idle_atlas = TextureAtlas::from_grid(idle_texture, Vec2::new(64.0, 64.0), 1, 1, None, None);
+    let idle = texture_atlases.add(idle_atlas);
+
+    let orc_assets = OrcAssets { idle };
+
+    commands.insert_resource(orc_assets);
+}
+
+fn spawn_orc_system(mut commands: Commands, mut events: EventReader<events::SpawnOrc>, orc_assets: Res<OrcAssets>) {
+    for event in events.iter() {
+        // TODO:
     }
 }
